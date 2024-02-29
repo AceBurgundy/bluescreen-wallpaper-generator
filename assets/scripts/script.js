2db9e3fe818cb3c4da8b2065d65844fa9fed0996
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-29 17:01:27 +0800

    Added files

---
+++ assets/scripts/script.js
@@ -0,0 +1,78 @@
+const emojiElement = document.getElementById("emoji");
+const titleElement = document.getElementById("title");
+const contentElement = document.getElementById("content");
+
+const messages = [
+  {
+    emoji: ":(",
+    title: "Your PC ran into a problem that it couldn't handle and now it needs to restart",
+    content: "You can search for the error online"
+  },
+  {
+    emoji: ":O",
+    title: "Your PC ran into a problem and needs to restart. We're just collecting some error info, and then we'll restart for you.",
+    content: ""
+  },
+  {
+    emoji: ":|",
+    title: "Your PC ran into a problem and needs to restart.",
+    content: "We're just collecting some error info, and then we'll restart for you."
+  },
+  {
+    emoji: ":'(",
+    title: "Your PC encountered an error and needs to restart.",
+    content: "We're just collecting some error info, and then we'll restart for you."
+  },
+  {
+    emoji: ":/",
+    title: "Your PC ran into a problem and needs to restart.",
+    content: "We're just collecting some error info, and then we'll restart for you."
+  },
+  {
+    user: true,
+    emoji: ':(',
+    title: 'A message to start with',
+    content: 'And some content?'
+  }
+];
+
+emojiElement.oninput = () => {
+  messages[messages.length - 1].emoji = emojiElement.value;
+}
+
+titleElement.oninput = () => {
+  messages[messages.length - 1].title = titleElement.value;
+  autoResizeTextarea(titleElement);
+}
+
+contentElement.oninput = () => {
+  messages[messages.length - 1].content = contentElement.value;
+}
+
+let index = 0;
+
+document.addEventListener("keydown", event => {
+  const left = event.key === "ArrowLeft";
+  const right = event.key === "ArrowRight";
+
+  if (left || right) {
+    index = left ? (index + 1) % messages.length : (index - 1 + messages.length) % messages.length;
+    const currentMessage = messages[index];
+
+    emojiElement.value = currentMessage.emoji;
+    titleElement.value = currentMessage.title;
+    contentElement.value = currentMessage.content;
+
+    autoResizeTextarea(titleElement);
+  }
+});
+
+// onload
+emojiElement.value = messages[messages.length - 1].emoji;
+titleElement.value = messages[messages.length - 1].title;
+contentElement.value = messages[messages.length - 1].content;
+
+function autoResizeTextarea(textarea) {
+  textarea.style.height = "auto";
+  textarea.style.height = (textarea.scrollHeight) + "px";
+}
